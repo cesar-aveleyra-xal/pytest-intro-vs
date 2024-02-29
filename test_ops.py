aaad6a630e589d56c6fbfde8231645cf29e942fa
cesar-aveleyra-xal <139596105+cesar-aveleyra-xal@users.noreply.github.com>
2024-02-29 09:44:59 -0600

    Update test_ops.py

--- test_ops.py
+++ test_ops.py
@@ -16,7 +16,7 @@ import os
 # Acceder a las variables de entorno
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-#print(aws_access_key_id)
-#print(aws_secret_access_key)
+print(aws_access_key_id)
+print(aws_secret_access_key)
 
 print('hola desde jenkins')
